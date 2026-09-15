77813507776ac6b9d0abba7a8388c896c1c5a421
Altynay Yertay <altynay.ertay@gmail.com>
2026-09-15 16:40:00 +0500

    Match reference shading direction and ellipse size

--- ay240782_a1/picture.cpp
+++ ay240782_a1/picture.cpp
@@ -78,7 +78,7 @@ void addShadedCircle(GLfloat cx, GLfloat cy, GLfloat radius, int segments)
         GLfloat angle = 2.0f * M_PI * i / segments;
         GLfloat x = cx + radius * cos(angle);
         GLfloat y = cy + radius * sin(angle);
-        GLfloat red = angle / (2.0f * M_PI);
+        GLfloat red = 1.0f - angle / (2.0f * M_PI);
         addVertex(x, y, red, 0.0f, 0.0f);
     }
 }
@@ -96,7 +96,7 @@ void addSquare(GLfloat cx, GLfloat cy, GLfloat radius, GLfloat shade)
 void buildScene()
 {
     ellipseStart = numVertices;
-    addEllipse(-0.6f, 0.73f, 0.2f, 0.12f, 1.0f, 0.0f, 0.0f, 60);
+    addEllipse(-0.6f, 0.73f, 0.24f, 0.144f, 1.0f, 0.0f, 0.0f, 60);
     ellipseCount = numVertices - ellipseStart;
 
     triangleStart = numVertices;
