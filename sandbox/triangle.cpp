7fc1b05c81af8fcc782bc7278c7ed4fc41e00106
Altynay Yertay <altynay.ertay@gmail.com>
2026-09-15 14:54:21 +0500

    Add triangle skeleton with empty init

--- sandbox/triangle.cpp
+++ sandbox/triangle.cpp
@@ -10,3 +10,44 @@
 
 #include <cstdlib>
 #include <cstdio>
+
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == 033 || key == 'q' || key == 'Q')
+        exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+
+#ifdef __APPLE__
+    glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE);
+#else
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitContextVersion(3, 2);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+#endif
+
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Step 2: Triangle");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
