e04f60bebf3d38a79e2537333131903d507ec3c4
Altynay Yertay <altynay.ertay@gmail.com>
2026-09-15 16:52:09 +0500

    Add creative.cpp with gradient background

---
+++ ay240782_a1/creative.cpp
@@ -0,0 +1,124 @@
+#ifdef __APPLE__
+  #define GL_SILENCE_DEPRECATION
+  #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+  #include <OpenGL/gl3.h>
+  #include <GLUT/glut.h>
+#else
+  #include <GL/glew.h>
+  #include <GL/freeglut.h>
+#endif
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include "InitShader.h"
+
+const int MAX_VERTICES = 2000;
+
+GLfloat vertices[MAX_VERTICES][4];
+GLfloat colors[MAX_VERTICES][4];
+int numVertices = 0;
+
+int backgroundStart, backgroundCount;
+
+void addVertex(GLfloat x, GLfloat y, GLfloat r, GLfloat g, GLfloat b)
+{
+    vertices[numVertices][0] = x;
+    vertices[numVertices][1] = y;
+    vertices[numVertices][2] = 0.0f;
+    vertices[numVertices][3] = 1.0f;
+
+    colors[numVertices][0] = r;
+    colors[numVertices][1] = g;
+    colors[numVertices][2] = b;
+    colors[numVertices][3] = 1.0f;
+
+    numVertices++;
+}
+
+void addBackground(int bands)
+{
+    for (int i = 0; i <= bands; i++) {
+        GLfloat t = (GLfloat)i / bands;
+        GLfloat y = 1.0f - 2.0f * t;
+        GLfloat r = 0.85f + 0.15f * t;
+        GLfloat g = 0.82f + 0.18f * t;
+        GLfloat b = 0.95f + 0.05f * t;
+        addVertex(-1.0f, y, r, g, b);
+        addVertex( 1.0f, y, r, g, b);
+    }
+}
+
+void buildScene()
+{
+    backgroundStart = numVertices;
+    addBackground(20);
+    backgroundCount = numVertices - backgroundStart;
+}
+
+void init()
+{
+    buildScene();
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(colors), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(colors), colors);
+
+    GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+    glUseProgram(program);
+
+    GLint vPosition = glGetAttribLocation(program, "vPosition");
+    glEnableVertexAttribArray(vPosition);
+    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+
+    GLint vColor = glGetAttribLocation(program, "vColor");
+    glEnableVertexAttribArray(vColor);
+    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid*)sizeof(vertices));
+
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLE_STRIP, backgroundStart, backgroundCount);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == 033 || key == 'q' || key == 'Q')
+        exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+
+#ifdef __APPLE__
+    glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE);
+#else
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitContextVersion(3, 2);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+#endif
+
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Assignment 1 - Task 2 Part 2");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
