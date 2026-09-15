7329daca5b53a86170d22ac5718ecca1486b7adc
Altynay Yertay <altynay.ertay@gmail.com>
2026-09-15 14:34:25 +0500

    Initial commit: window and triangle sandbox

---
+++ sandbox/window.cpp
@@ -0,0 +1,47 @@
+#ifdef __APPLE__
+  #define GL_SILENCE_DEPRECATION
+  #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+  #include <OpenGL/gl3.h>
+  #include <GLUT/glut.h>
+#else
+  #include <GL/glew.h>
+  #include <GL/freeglut.h>
+#endif
+
+#include <cstdlib>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == 033 || key == 'q' || key == 'Q')
+        exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+
+#ifdef __APPLE__
+    glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE);
+#else
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitContextVersion(3, 2);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+#endif
+
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Step 1");
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
