e8403e0e94bf72c218e0624dfcd60da6958c1da0
Altynay Yertay <altynay.ertay@gmail.com>
2026-09-15 14:49:58 +0500

    Add includes for triangle program

---
+++ sandbox/triangle.cpp
@@ -0,0 +1,12 @@
+#ifdef __APPLE__
+  #define GL_SILENCE_DEPRECATION
+  #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+  #include <OpenGL/gl3.h>
+  #include <GLUT/glut.h>
+#else
+  #include <GL/glew.h>
+  #include <GL/freeglut.h>
+#endif
+
+#include <cstdlib>
+#include <cstdio>
